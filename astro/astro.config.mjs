ee87f993ca62ff6a201219cc4b2bafb3de647d89
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-11-25 14:13:09 +0530

    ..

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,17 +1,16 @@
 // @ts-check
-import { defineConfig,envField  } from 'astro/config';
-
+import { defineConfig, envField } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from '@tailwindcss/vite';
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   env: {
     schema: {
-      VITE_EMAIL_SERVICE_ID: envField.string({ context: "client", access: "public"}),
-      VITE_EMAIL_TEMPLATE_ID: envField.string({ context: "client", access: "public"}),
-      VITE_EMAIL_PUBLIC_KEY: envField.string({ context: "client", access: "public"}),
+      VITE_EMAIL_SERVICE_ID: envField.string({ context: "client", access: "public" }),
+      VITE_EMAIL_TEMPLATE_ID: envField.string({ context: "client", access: "public" }),
+      VITE_EMAIL_PUBLIC_KEY: envField.string({ context: "client", access: "public" }),
     }
   },
 
@@ -19,5 +18,7 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(),sitemap()],
+
+  site: "https://tanmoymondal.pages.dev",
 });
